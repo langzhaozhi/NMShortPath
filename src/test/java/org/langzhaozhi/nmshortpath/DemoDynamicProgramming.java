cb37358c97d7a508038dc859f99e6f718604039c
langzhaozhi <langzhaozhi@sina.cn>
2015-02-06 15:55:15 +0800

    完善测试用例

--- src/test/java/org/langzhaozhi/nmshortpath/DemoDynamicProgramming.java
+++ src/test/java/org/langzhaozhi/nmshortpath/DemoDynamicProgramming.java
@@ -18,18 +18,20 @@ import java.util.concurrent.atomic.AtomicInteger;
  * <p>效果还好，算法本身已经到单线程求解的极限，要更快的话只能以后考虑实现并发求解N-最短路径的算法</p>
  */
 public class DemoDynamicProgramming {
-    private static final int ColumnCount = 300;
+    private static final int ColumnCount = 500;
     private static final int VertexCountOfEveryColumn = 100;
-    private static final int N = 100;//NM-最短路径的N可以依次调 N=1,2,3.....看下情况;
-    private static final int M = 100;//当M调节的愈来愈大的时候，可以明显看到组合爆炸的问题：表现为内存占用和求解时间的爆炸伤
+    private static final int N = 10;//NM-最短路径的N可以依次调 N=1,2,3.....看下情况;
+    private static final int M = 10000000;//当M调节的愈来愈大的时候，可以明显看到组合爆炸的问题：表现为内存占用和求解时间的爆炸伤
 
     public static void main(String [] args) {
         AtomicInteger attachmentGenerator = new AtomicInteger( 0 );
 
-        //DebugDistanceGenerator gen = DemoDynamicProgramming.mFixedGenerator;//看组合爆炸,必须由M来压制
-        DebugDistanceGenerator gen = DemoDynamicProgramming.mDecrementGenerator;//M无影响
+        //DebugDistanceGenerator gen = DemoDynamicProgramming.mFixedGenerator;//所有边的距离相同：看组合爆炸,必须由M来压制
+        //DebugDistanceGenerator gen = DemoDynamicProgramming.mVertexSameGenerator;//任意两顶点的前向距离相同，但一个顶点的各前向边不同
+        //DebugDistanceGenerator gen = DemoDynamicProgramming.mColumnSameGenerator;
+        DebugDistanceGenerator gen = DemoDynamicProgramming.mAllNotSameGenerator;//固定N后 M影响趋向于0
 
-        NMShortPathGraph<Integer> graphModel = new NMShortPathGraph<Integer>( DemoDynamicProgramming.N, DemoDynamicProgramming.M, attachmentGenerator.getAndIncrement(), 0xFFFF_FFFF );
+        NMShortPathGraph<Integer> graphModel = new NMShortPathGraph<Integer>( N, M, attachmentGenerator.getAndIncrement(), 0xFFFF_FFFF );
 
         //初始<#起点#>为前一排的顶点
         ArrayList<NMShortPathVertex<Integer>> previousColumnVertexArray = new ArrayList<NMShortPathVertex<Integer>>( DemoDynamicProgramming.VertexCountOfEveryColumn );
@@ -63,7 +65,7 @@ public class DemoDynamicProgramming {
         System.out.println( "求解动态规划 spend: " + (t2 - t1) + " ms\n\n" );
         System.out.flush();
         //打印看下结果
-        System.err.println( "NM-最短路径(N==" + DemoDynamicProgramming.N + ", M=" + DemoDynamicProgramming.M + "):" );
+        System.err.println( "NM-最短路径(N==" + N + ", M=" + M + "):" );
         System.err.println( "    实际ShortPath个数(N)为[" + resultNMShortPath.getShortPathCount() + "]个" );
         System.err.println( "    实际VertexPath个数(M)所有从起点到终点的经由不同顶点的路径顶点序列有[" + resultNMShortPath.getVertexPathCount() + "]个" );
         /*
@@ -88,7 +90,8 @@ public class DemoDynamicProgramming {
         */
     }
 
-    static AtomicInteger mDistanceGenerator = new AtomicInteger( 1000_0000 );
+    static final int mColumnCounter = DemoDynamicProgramming.ColumnCount * DemoDynamicProgramming.VertexCountOfEveryColumn;
+    static AtomicInteger mDistanceGenerator = new AtomicInteger( DemoDynamicProgramming.mColumnCounter * 1000 );
 
     @FunctionalInterface
     interface DebugDistanceGenerator {
@@ -96,15 +99,37 @@ public class DemoDynamicProgramming {
     }
 
     /**
-     * 所有边的距离都是1.0：此时哪怕N==1 也产生组合爆炸问题：超指数增长的不同顶点路径的组合数目增长。
-     * 此时可把M设置成很大或 Integer.MAX_VALUE 来看 OOM 的情况
+     * 所有边的距离都是0.7：此时哪怕 N==1 也产生组合爆炸问题：超指数增长的不同顶点路径的组合数目增长。
+     * 此时固定N==1，此时可把 M 逐步调大来看爆炸的影响。如果N为其他数，那爆炸的更快了。
      */
-    public static DebugDistanceGenerator mFixedGenerator = () -> 1.0;
+    public static DebugDistanceGenerator mFixedGenerator = () -> 0.7;
 
     /**
-     * 所有边的距离都不相同，此时 M 没有什么影响，因为每一个ShortPath 基本只有一条 VertexPath。
-     *
-     * 问题取决于 N 的大小, 此时把 M 设置成多大也没有什么影响
+     * 一列内部各顶点相同，但一个顶点的各个边不同：固定N后看 M 的逐步影响
      */
-    public static DebugDistanceGenerator mDecrementGenerator = () -> DemoDynamicProgramming.mDistanceGenerator.getAndDecrement();
+    public static DebugDistanceGenerator mVertexSameGenerator = () -> {
+        int distance = DemoDynamicProgramming.mDistanceGenerator.decrementAndGet();
+        if ((distance % (DemoDynamicProgramming.VertexCountOfEveryColumn)) == 0) {
+            DemoDynamicProgramming.mDistanceGenerator.set( DemoDynamicProgramming.mColumnCounter * 1000 );
+        }
+        return distance + 0.7;
+    };
+
+    /**
+     * 每一列的顶点的边距离相同，但一列内部的各顶点不相同：固定N后看 M 的逐步影响
+     */
+    public static DebugDistanceGenerator mColumnSameGenerator = () -> {
+        int distance = DemoDynamicProgramming.mDistanceGenerator.decrementAndGet();
+        if ((distance % (DemoDynamicProgramming.mColumnCounter)) == 0) {
+            DemoDynamicProgramming.mDistanceGenerator.set( DemoDynamicProgramming.mColumnCounter * 1000 );
+        }
+        return distance + 0.7;
+    };
+
+    /**
+     * 所有边的距离都不相同：此时固定N后， M 几乎没有什么影响，因为每一个ShortPath 基本只有一条 VertexPath。
+     *
+     * 问题能否求解（发生OOM）取决于 N 的大小, 此时把 M 设置成多大影响也不大。
+     */
+    public static DebugDistanceGenerator mAllNotSameGenerator = () -> DemoDynamicProgramming.mDistanceGenerator.getAndDecrement() + 0.7;
 }
